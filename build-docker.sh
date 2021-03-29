4010740fd41325f56e3a38541b0b656d02533c10
JunHCha <quffl9612@naver.com>
2021-03-29 12:13:50 +0900

    fix: delete run.sh transfer code in build-docker

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,2 @@
-docker build -t cse364-ubuntu20.04/movie_recommender_os
-docker run -d --name cont cse364-ubuntu20.04/movie_recommender_os 
-
-docker cp ./run.sh ml1:/
+docker build -t cse364-ubuntu20.04/movie_recommender_os .
+winpty docker run -it --name container cse364-ubuntu20.04/movie_recommender_os
