0662f1d381ee8bf132885bec085484d1a0377f48
JunHCha <quffl9612@naver.com>
2021-03-29 18:43:11 +0900

    feat: fix build-docker to fit ubuntu

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,2 @@
 docker build -t cse364-ubuntu20.04/movie_recommender_os .
-winpty docker run -it --name container cse364-ubuntu20.04/movie_recommender_os
+docker run -it --name container cse364-ubuntu20.04/movie_recommender_os
