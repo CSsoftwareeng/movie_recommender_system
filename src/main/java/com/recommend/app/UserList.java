685efbe1383647dc3b2b01134d8f9512c0dbbf45
clubzip <ns4809@unist.ac.kr>
2021-04-22 14:44:46 +0900

    feat: overload build method for gender, age, and occupation

--- src/main/java/com/recommend/app/UserList.java
+++ src/main/java/com/recommend/app/UserList.java
@@ -30,6 +30,22 @@ public class UserList {
         } catch (IOException e) {}
     }
 
+    void build(char gender, int age, int occupation) throws UserNotExistError {
+
+        try {
+            File usersFile = new File("./data/users.dat");
+            FileReader reader = new FileReader(usersFile);
+            BufferedReader buffer = new BufferedReader(reader);
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                String[] user = line.split("::");
+                if (user[1].charAt(0) == gender  && Integer.parseInt(user[2]) == age && Integer.parseInt(user[3]) == occupation) {
+                    users.add(Integer.parseInt(user[0]));
+                }
+            }
+        } catch (IOException e) {}
+    }
+
     public boolean find(int occupation) {
         return users.contains(occupation);
     }
