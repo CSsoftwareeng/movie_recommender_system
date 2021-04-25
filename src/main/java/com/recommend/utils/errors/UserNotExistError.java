e73b85d7d903707cf17fbc556c2d43b7353f394b
clubzip <52950649+clubzip@users.noreply.github.com>
2021-04-25 18:18:49 +0900

    fix: error message

--- src/main/java/com/recommend/utils/errors/UserNotExistError.java
+++ src/main/java/com/recommend/utils/errors/UserNotExistError.java
@@ -8,7 +8,7 @@ public class UserNotExistError extends IllegalArgumentException{
     }
 
     public void errMessage() {
-        System.out.println("Can't find any user whose occupation matchs");
+        System.out.println("Can't find any corresponding user.");
         System.out.println("-------------Program was forced to exit.-------------\n");
         System.exit(0);
     }
