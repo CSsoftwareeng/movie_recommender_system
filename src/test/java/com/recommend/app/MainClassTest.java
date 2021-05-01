7675c3d5ebcabd2daf6ecaf9c201113c0b6fd950
JunHCha <quffl9612@naver.com>
2021-05-02 03:42:04 +0900

    feat: add ArgCntError test code

--- src/test/java/com/recommend/app/MainClassTest.java
+++ src/test/java/com/recommend/app/MainClassTest.java
@@ -1,4 +1,23 @@
 package com.recommend.app;
 
+import com.recommend.utils.errors.ArgCntError;
+import java.util.*;
+import jdk.jfr.Timestamp;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class MainClassTest {
+
+  @Test(expected = ArgCntError.class)
+  public void testArgCntErrorUnder() {
+    String[] args1 = { "1", "2" };
+    MainClass.main(args1);
+  }
+
+  @Test(expected = ArgCntError.class)
+  public void testArgCntErrorOver() {
+    String[] args1 = { "1", "2", "3", "4", "5" };
+    MainClass.main(args1);
+  }
 }
