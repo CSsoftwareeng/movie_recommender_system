8aa12a389b4904df239efe06181fe0b05a2f7dc7
JunHCha <quffl9612@naver.com>
2021-05-02 03:30:04 +0900

    fix: apply code formatter in mainclass

--- src/main/java/com/recommend/app/MainClass.java
+++ src/main/java/com/recommend/app/MainClass.java
@@ -3,19 +3,24 @@ package com.recommend.app;
 import com.recommend.utils.errors.ArgCntError;
 
 public class MainClass {
-    public static void main(String[] args) throws ArgCntError {
-        if (args.length <= 2 || args.length > 4) {
-            throw new ArgCntError(args, args.length);
-        }
 
-        Arguments arg = args.length == 3 ? new Arguments(args[0], args[1], args[2]) : new Arguments(args[0], args[1], args[2], args[3]);
-        arg.printArgs();
-        MovieList movieList = new MovieList(arg.getGenres());
-
-        UserList userlist = new UserList();
-        userlist.searchSimilarUser(arg.getGender(), arg.getAge(), arg.getOccupation());
-        RatingCalculator rating = new RatingCalculator(movieList,userlist);
-        rating.showResult(movieList);
+  public static void main(String[] args) throws ArgCntError {
+    if (args.length <= 2 || args.length > 4) {
+      throw new ArgCntError(args, args.length);
     }
-}
+    Arguments arg = args.length == 3
+      ? new Arguments(args[0], args[1], args[2])
+      : new Arguments(args[0], args[1], args[2], args[3]);
+    arg.printArgs();
+    MovieList movieList = new MovieList(arg.getGenres());
 
+    UserList userlist = new UserList();
+    userlist.searchSimilarUser(
+      arg.getGender(),
+      arg.getAge(),
+      arg.getOccupation()
+    );
+    RatingCalculator rating = new RatingCalculator(movieList, userlist);
+    rating.showResult(movieList);
+  }
+}
