806974f5d5255bcc8d9650e6948dddcca96e8785
clubzip <ns4809@unist.ac.kr>
2021-05-02 15:20:14 +0000

    fix: handle ArgNotExistError

--- src/main/java/com/recommend/app/MainClass.java
+++ src/main/java/com/recommend/app/MainClass.java
@@ -8,11 +8,12 @@ public class MainClass {
     if (args.length <= 2 || args.length > 4) {
       throw new ArgCntError(args, args.length);
     }
-    Arguments arg = args.length == 3
-      ? new Arguments(args[0], args[1], args[2])
-      : new Arguments(args[0], args[1], args[2], args[3]);
 
     try {
+      Arguments arg = args.length == 3
+        ? new Arguments(args[0], args[1], args[2])
+        : new Arguments(args[0], args[1], args[2], args[3]);
+
       MovieList movieList = new MovieList(arg.getGenres());
       UserList userlist = new UserList();
       userlist.searchSimilarUser(
@@ -22,6 +23,6 @@ public class MainClass {
       );
       RatingCalculator rating = new RatingCalculator(movieList, userlist);
       rating.showResult(movieList);
-    } catch (MovieNotExistError e) {}
+    } catch (MovieNotExistError e) {} catch (ArgNotExistError e) {}
   }
 }
