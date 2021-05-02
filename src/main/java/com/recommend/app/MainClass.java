92243f0205793423d7a9431a26a486b62c63181f
clubzip <ns4809@unist.ac.kr>
2021-05-02 14:35:20 +0000

    feat: add MainClass test

--- src/main/java/com/recommend/app/MainClass.java
+++ src/main/java/com/recommend/app/MainClass.java
@@ -1,6 +1,6 @@
 package com.recommend.app;
 
-import com.recommend.utils.errors.ArgCntError;
+import com.recommend.utils.errors.*;
 
 public class MainClass {
 
@@ -8,19 +8,20 @@ public class MainClass {
     if (args.length <= 2 || args.length > 4) {
       throw new ArgCntError(args, args.length);
     }
-
     Arguments arg = args.length == 3
       ? new Arguments(args[0], args[1], args[2])
       : new Arguments(args[0], args[1], args[2], args[3]);
-    MovieList movieList = new MovieList(arg.getGenres());
-
-    UserList userlist = new UserList();
-    userlist.searchSimilarUser(
-      arg.getGender(),
-      arg.getAge(),
-      arg.getOccupation()
-    );
-    RatingCalculator rating = new RatingCalculator(movieList, userlist);
-    rating.showResult(movieList);
+    
+    try {
+      MovieList movieList = new MovieList(arg.getGenres());
+      UserList userlist = new UserList();
+      userlist.searchSimilarUser(
+        arg.getGender(),
+        arg.getAge(),
+        arg.getOccupation()
+      );
+      RatingCalculator rating = new RatingCalculator(movieList, userlist);
+      rating.showResult(movieList);
+    } catch (MovieNotExistError e) {}
   }
 }
--- src/test/java/com/recommend/app/MainClassTest.java
+++ src/test/java/com/recommend/app/MainClassTest.java
@@ -3,9 +3,8 @@ package com.recommend.app;
 import com.recommend.utils.errors.ArgCntError;
 import java.util.*;
 import jdk.jfr.Timestamp;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
+import java.io.*;
 
 public class MainClassTest {
 
@@ -19,5 +18,27 @@ public class MainClassTest {
   public void testArgCntErrorOver() {
     String[] args1 = { "1", "2", "3", "4", "5" };
     MainClass.main(args1);
+  }
+  
+  @Test
+  public void testMainClassFourInputs() {
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    PrintStream originalOutput = System.out;
+    String[] args1 = { "F", "25", "Grad student", "Action|Comedy" };
+    System.setOut(new PrintStream(output));
+    MainClass.main(args1);
+    Assert.assertTrue(output.toString().contains("Dead Presidents (1995) (http://www.imdb.com/title/tt0112819) Rating : 5.0"));
+    System.setOut(originalOutput);
+  }
+
+  @Test
+  public void testMainClassThreeInputs() {
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    PrintStream originalOutput = System.out;
+    String[] args1 = { "F", "25", "Grad student" };
+    System.setOut(new PrintStream(output));
+    MainClass.main(args1);
+    Assert.assertTrue(output.toString().contains("Chungking Express (1994) (http://www.imdb.com/title/tt0109424) Rating : 5.0"));
+    System.setOut(originalOutput);
   }
 }
