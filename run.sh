2e222d622766b57a44f6a69c8f08c3dce5686511
JunHCha <quffl9612@naver.com>
2021-05-18 01:07:06 +0900

    feat: fix run.sh to run spring web project

--- run.sh
+++ run.sh
@@ -2,11 +2,5 @@
 
 git clone https://github.com/CSsoftwareeng/movie_recommender_system.git
 cd movie_recommender_system
-mvn install
-java -cp target/cse364-project-1-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.app.MainClass "F" "25" "Grad student" "Action|Comedy"
-java -cp target/cse364-project-1-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.app.MainClass "M" "30" "Athletes" "Children’s"
-java -cp target/cse364-project-1-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.app.MainClass "F" "" "retired" "Animation|Drama|Fantasy" 
-java -cp target/cse364-project-1-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.app.MainClass "" "15" "" "Sci-Fi"
-java -cp target/cse364-project-1-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.app.MainClass "" "" "" "Romance|Comedy"
-
-
+mvn package
+java -jar target/cse364-project-1-1.0-SNAPSHOT.jar
