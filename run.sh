943d4ebcd9e2be19d522c2fad5c58697e448299e
clubzip <52950649+clubzip@users.noreply.github.com>
2021-05-24 01:25:51 +0900

    fix: fix run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 
 git clone https://github.com/CSsoftwareeng/movie_recommender_system.git
 cd movie_recommender_system
-mvn package
-java -jar target/cse364-project-1-1.0-SNAPSHOT.jar
+mvn spring-boot:run
