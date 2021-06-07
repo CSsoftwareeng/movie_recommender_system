878fb599d755ac4ed4209257764b30c900212ad5
JunHCha <quffl9612@naver.com>
2021-06-07 14:32:22 +0900

    feat: apply imdb link to cards

--- src/main/resources/public/js/movieBased.js
+++ src/main/resources/public/js/movieBased.js
@@ -14,13 +14,17 @@ function btn_movie_click() {
     url: "/movies/recommendations",
   }).then(function (data) {
     for (var i = 0; i < Object.keys(data).length; i++) {
+      $link = $(
+        '<a href="' +
+          data[i].imdb.substring(1, data[i].imdb.length - 1) +
+          '" target="_blank"></a>'
+      );
       $card = $('<div class="card"></div>');
-      $card_body = $('<div class="card-body"></div>');
       $card_text = $('<p class="card-text"></div>');
       $card_text.html(data[i].title);
-      $card_body.append($card_text);
-      $card.append($card_body);
-      $result.append($card);
+      $card.append($card_text);
+      $link.append($card);
+      $result.append($link);
     }
     $(".contents").append($result);
   });
