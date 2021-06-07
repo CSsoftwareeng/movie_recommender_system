2cc63d8bd7a2350fc129e0a7927d1ca53c2796c6
JunHCha <quffl9612@naver.com>
2021-06-08 00:40:05 +0900

    chore: fix syntax and incomplete logic to run temp

--- src/main/java/com/recommend/app/MovieList.java
+++ src/main/java/com/recommend/app/MovieList.java
@@ -128,39 +128,39 @@ public class MovieList {
   }
 
   public void searchAllMovies() {
-    try {
-      File moviefile = new File("./data/movies.dat");
-      FileReader fileReader = new FileReader(moviefile);
-      BufferedReader bufReader = new BufferedReader(fileReader);
-      String data = "";
-      while ((data = bufReader.readLine()) != null) {
-        String[] temp = data.split("::");
-        this.titles.put(Integer.parseInt(temp[0]), temp[1]);
-        this.genres.put(Integer.parseInt(temp[0]), temp[2]);
-      }
-    } catch (IOException e) {}
+    // try {
+    //   File moviefile = new File("./data/movies.dat");
+    //   FileReader fileReader = new FileReader(moviefile);
+    //   BufferedReader bufReader = new BufferedReader(fileReader);
+    //   String data = "";
+    //   while ((data = bufReader.readLine()) != null) {
+    //     String[] temp = data.split("::");
+    //     this.titles.put(Integer.parseInt(temp[0]), temp[1]);
+    //     this.genres.put(Integer.parseInt(temp[0]), temp[2]);
+    //   }
+    // } catch (IOException e) {}
 
-    try {
-      File linkfile = new File("./data/links.dat");
-      FileReader fileReader = new FileReader(linkfile);
-      BufferedReader bufReader = new BufferedReader(fileReader);
-      String data = "";
-      while ((data = bufReader.readLine()) != null) {
-        String[] temp = data.split("::");
-        String movietitle = "";
-        String moviegenre = "";
-        String movielink = "";
-        movietitle = this.titles.get(Integer.parseInt(temp[0]));
-        moviegenre = this.genres.get(Integer.parseInt(temp[0]));
-        movielink = temp[1];
-        Movie movie = new Movie(
-                movietitle,
-                moviegenre,
-                "(http://www.imdb.com/title/tt" + movielink + ")"
-        );
-        this.moviesResult.add(movie);
-      }
-    } catch (IOException e) {}
+    // try {
+    //   File linkfile = new File("./data/links.dat");
+    //   FileReader fileReader = new FileReader(linkfile);
+    //   BufferedReader bufReader = new BufferedReader(fileReader);
+    //   String data = "";
+    //   while ((data = bufReader.readLine()) != null) {
+    //     String[] temp = data.split("::");
+    //     String movietitle = "";
+    //     String moviegenre = "";
+    //     String movielink = "";
+    //     movietitle = this.titles.get(Integer.parseInt(temp[0]));
+    //     moviegenre = this.genres.get(Integer.parseInt(temp[0]));
+    //     movielink = temp[1];
+    //     Movie movie = new Movie(
+    //             movietitle,
+    //             moviegenre,
+    //             "(http://www.imdb.com/title/tt" + movielink + ")"
+    //     );
+    //     this.moviesResult.add(movie);
+    //   }
+    // } catch (IOException e) {}
   }
 
   public List getMoviesResult() {
