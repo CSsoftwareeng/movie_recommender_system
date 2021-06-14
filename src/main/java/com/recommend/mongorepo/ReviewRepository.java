88030f8b3cb2ed7029a078ce3b1e5195ccba7ede
clubzip <ns4809@unist.ac.kr>
2021-06-14 14:26:45 +0000

    feat: searchFavorite using db

--- src/main/java/com/recommend/mongorepo/ReviewRepository.java
+++ src/main/java/com/recommend/mongorepo/ReviewRepository.java
@@ -9,9 +9,12 @@ import org.springframework.data.mongodb.repository.Query;
 
 public interface ReviewRepository extends MongoRepository<Review, String> {
 
-  public List<Review> findByUserid(int userid);
-  public List<Review> findByMovieid(int movieid);
+  public List<Review> findByUserid(int userid); //sort 필요.
+  public List<Review> findByMovieidOrderByUseridAsc(int movieid);
   public List<Review> findByRating(int rating);
   public List<Review> findByDate(int date);
+
+  @Aggregation({"{ '$match': { 'userid': {'$in': ?0 } } }", "{ '$group': { '_id': '$userid', 'avgRating': { '$avg': '$rating' } } }", "{ '$sort': { '_id': 1 } }", "{ '$project': { '_id': '$avgRating' } }"})
+  List<Double> findAverageRatingByUserid(List<Integer> users);
 
 }
