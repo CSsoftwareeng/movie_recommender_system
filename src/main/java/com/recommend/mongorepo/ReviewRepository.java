56b859d0de65533dd6859aefed232ea9192894e8
clubzip <ns4809@unist.ac.kr>
2021-06-15 10:21:22 +0000

    feat: use mongodb for data

--- src/main/java/com/recommend/mongorepo/ReviewRepository.java
+++ src/main/java/com/recommend/mongorepo/ReviewRepository.java
@@ -1,7 +1,7 @@
 package com.recommend.app;
 
 import java.util.List;
-
+import java.util.TreeSet;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.Query;
@@ -16,5 +16,7 @@ public interface ReviewRepository extends MongoRepository<Review, String> {
 
   @Aggregation({"{ '$match': { 'userid': {'$in': ?0 } } }", "{ '$group': { '_id': '$userid', 'avgRating': { '$avg': '$rating' } } }", "{ '$sort': { '_id': 1 } }", "{ '$project': { '_id': '$avgRating' } }"})
   List<Double> findAverageRatingByUserid(List<Integer> users);
+  @Query(value = "{'userid': {'$in' : ?0 } }", fields = "{'_id': 0}")
+  List<Review> findByUseridIn(TreeSet<Integer> users);
 
 }
